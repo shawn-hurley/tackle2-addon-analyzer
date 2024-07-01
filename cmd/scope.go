a77b13270d68cb41d37548c9bcb8f478f2707ecf
Shawn Hurley <shawn@hurley.page>
2024-07-01 14:52:31 -0400

    don't run deps anaylsis when source only
    Signed-off-by: Shawn Hurley <shawn@hurley.page>

--- cmd/scope.go
+++ cmd/scope.go
@@ -17,10 +17,17 @@ type Scope struct {
 
 // AddOptions adds analyzer options.
 func (r *Scope) AddOptions(options *command.Options, mode Mode) (err error) {
-	if !r.WithKnownLibs && mode.WithDeps {
-		options.Add(
-			"--dep-label-selector",
-			"!konveyor.io/dep-source=open-source")
+	// If withDeps is false, we are only every doing source analysis
+	// adding a dep label selector is strictly wrong in this situation
+	if mode.WithDeps {
+		// We want to filter out open source violations when we are not running
+		// with known libraries.
+		if !r.WithKnownLibs {
+			options.Add(
+				"--dep-label-selector",
+				"!konveyor.io/dep-source=open-source")
+		}
+
 	}
 	selector := r.incidentSelector()
 	if selector != "" {
